25dda6f934e6a015c8ec5b143c0226468e076517
szocei <szocei.zoltan@gmail.com>
2020-11-25 21:42:05 +0100

    1.feladat kész

---
+++ string_1_feladat.js
@@ -0,0 +1,17 @@
+const johndoe = 
+{
+Firstname: 'John',
+lastname: 'Doe'
+};
+const janedoe = 
+{
+Firstname: 'Jane',
+lastname: 'Doe'  
+};
+const nagyjozsi={
+    Firstname: 'Józsi',
+    lastname: 'Nagy'
+}
+const objecteredeti=[johndoe, janedoe, nagyjozsi ]
+const egybe= objecteredeti.map((item, index)=> ({item}));
+ console.log(egybe);
